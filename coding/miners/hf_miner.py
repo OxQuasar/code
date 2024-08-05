1bc91e5db96e2410f510521c2c12a35e1a388760
0xquasar <0xquasar@gmail.com>
2024-08-05 11:02:53 -0700

    hf

--- coding/miners/hf_miner.py
+++ coding/miners/hf_miner.py
@@ -1,8 +1,8 @@
 from typing import Awaitable
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-from coding.protocol import CodeSynapse
-from langchain_huggingface.llms import HuggingFacePipeline
+from coding.protocol import StreamCodeSynapse
+#from langchain_huggingface.llms import HuggingFacePipeline
 
 
 def miner_init(self):
@@ -11,18 +11,19 @@ def miner_init(self):
     """
 
     self.tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-1.3b-base", trust_remote_code=True)
-    self.model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-1.3b-base", trust_remote_code=True).cuda()
+    self.model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-1.3b-base", trust_remote_code=True)#.cuda()
 
 
     
-def miner_process(self, synapse: CodeSynapse) -> Awaitable:
+def miner_process(self, synapse: StreamCodeSynapse) -> Awaitable:
     """
     The miner process function is called every time the miner receives a request. This function should contain the main logic of the miner.
     """
     prompt = f'<｜fim▁begin｜>{synapse.query}<｜fim▁end｜>'
-    
+
     inputs = self.tokenizer(prompt, return_tensors="pt").to(self.model.device)
-    synapse.completion = self.model.generate(**inputs, max_length=1024)[0]
+    outputs = self.model.generate(**inputs, max_length=1024)[0]
+    synapse.completion = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
 
 
     return synapse
